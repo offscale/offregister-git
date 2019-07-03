175ba5781deaa1c7840a9ef18b829b5333a767c6
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2019-07-04 09:50:47 +1000

    Exposed skip_clean

--- offregister_git/ubuntu/__init__.py
+++ offregister_git/ubuntu/__init__.py
@@ -11,6 +11,9 @@ logger = get_logger(modules[__name__].__name__)
 def push0(**kwargs):
     apt_depends('git')
 
-    clone_or_update(repo=kwargs['GIT_REPO'], to_dir=kwargs['GIT_DIR'], use_sudo=kwargs.get('use_sudo', False),
-                    branch=kwargs.get('GIT_BRANCH', 'master'), skip_reset=kwargs.get('skip_reset', False),
+    clone_or_update(repo=kwargs['GIT_REPO'], to_dir=kwargs['GIT_DIR'],
+                    use_sudo=kwargs.get('use_sudo', False),
+                    branch=kwargs.get('GIT_BRANCH', 'master'),
+                    skip_reset=kwargs.get('skip_reset', False),
+                    skip_clean=kwargs.get('skip_clean', True),
                     reset_to_first=kwargs.get('GIT_RESET_TO_FIRST', False))
